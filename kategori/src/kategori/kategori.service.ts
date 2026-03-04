ca36af913b6996b9f6a53d7fafff9d6565051ab8
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-03-04 17:29:59 +0700

    feat: add pbs if 23 d

---
+++ kategori/src/kategori/kategori.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateKategoriDto } from './dto/create-kategori.dto';
+import { UpdateKategoriDto } from './dto/update-kategori.dto';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class KategoriService {
+  // buat constructor untuk inject prisma service
+  constructor(private readonly prisma: PrismaService) {}
+  create(createKategoriDto: CreateKategoriDto) {
+    return 'This action adds a new kategori';
+  }
+
+  findAll() {
+    return `Raditya Ahmad`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} kategori`;
+  }
+
+  update(id: number, updateKategoriDto: UpdateKategoriDto) {
+    return `This action updates a #${id} kategori`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} kategori`;
+  }
+}
